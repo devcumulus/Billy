c8b6d556c540bed1efdc84d640346804e759aabc
devcumulus <dev.cumulus@gmail.com>
2024-01-15 16:58:15 +0900

    feat : Breadcrumb 컴포넌트추가 , DetailsPage 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Loading from "./components/loading/Loading";
+import Breadcrumb from "./components/breadcrumb/Breadcrumb";
+import "antd/dist/antd.css";
 
 const LazyJoinPage = lazy(() => import("./pages/join/JoinPage"));
 const LazyLoginPage = lazy(() => import("./pages/login/LoginPage"));
@@ -13,59 +15,72 @@ const LazyMainPage = lazy(() => import("./pages/main/MainPage"));
 
 const LazyPayPage = lazy(() => import("./pages/pay/PayPage"));
 
+const LazyDetailsPage = lazy(() => import("./pages/details/DetailsPage"));
+
 const App = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route
-          path="main"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyMainPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/login"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyLoginPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/join/"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyJoinPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/my/"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyMyPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/addedit/"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyAddEditPage />
-            </Suspense>
-          }
-        ></Route>
-        <Route
-          path="/pay/"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyPayPage />
-            </Suspense>
-          }
-        ></Route>
-      </Routes>
+      <Breadcrumb />
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route
+            path="main"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyMainPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/login"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyLoginPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/join/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyJoinPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/my/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyMyPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/addedit/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyAddEditPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/pay/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyPayPage />
+              </Suspense>
+            }
+          ></Route>
+          <Route
+            path="/details/"
+            element={
+              <Suspense fallback={<Loading />}>
+                <LazyDetailsPage />
+              </Suspense>
+            }
+          ></Route>
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
--- src/pages/main/MainPage.js
+++ src/pages/main/MainPage.js
@@ -1,12 +1,7 @@
 import React from "react";
-import Layout from "../../layouts/Layout";
 
 const MainPage = () => {
-  return (
-    <Layout>
-      <h1>MainPage</h1>
-    </Layout>
-  );
+  return <div>MainPage</div>;
 };
 
 export default MainPage;
